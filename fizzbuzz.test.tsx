eb7660f27753412cf0657f859a5aca3180f94c33
0xUndersun <alex.atc.dev@gmail.com>
2023-07-18 00:19:34 +0200

    2/4 test success,  miss 3 && 5 or display numberToCheck.

--- fizzbuzz.test.tsx
+++ fizzbuzz.test.tsx
@@ -4,4 +4,7 @@ describe("Fizzbuzz", () => {
   it("Renvoi Fizz si le nombre est divisible par 3", () => {
     expect(Fizzbuzz(3)).toBe("Fizz");
   });
+  it("Renvoi Buzz si le nombre est divisible par 5", () => {
+    expect(Fizzbuzz(5)).toBe("Buzz");
+  });
 });
--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -2,5 +2,8 @@ export function Fizzbuzz(numberToCheck: number): string {
   if (numberToCheck % 3 === 0) {
     return "Fizz";
   }
+  if (numberToCheck % 5 === 0) {
+    return "Buzz";
+  }
   return numberToCheck.toString();
 }
